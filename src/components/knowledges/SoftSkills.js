d45c3084059a1deca7f6f00af6756f615b15fd5f
Stéphane PIERRE <stephane_pierre@hotmail.fr>
2021-04-22 15:26:26 +0200

    STYLE responsive for knowledges page

--- src/components/knowledges/SoftSkills.js
+++ src/components/knowledges/SoftSkills.js
@@ -15,22 +15,22 @@ const SoftSkills = () => {
         <li className="softSkillsContent__list">
           <img src={ Visualisation } alt="visualisation" />
           <h6>Visualisation:</h6> 
-          <p className="text">Je me projette pour établir une stratégie adaptée.</p>
+          <p className="text">Je me projette pour établir une stratégie adaptée. La vente m'a permit de développer cette aptitude pour pouvoir répondre aux objections des clients.</p>
         </li>
         <li className="softSkillsContent__list">
           <img src={ Collectif } alt="collectif" />
           <h6>Sens du collectif:</h6>
-          <p className="text">Faire confiance, se soutenir.</p>
+          <p className="text">Faire confiance, se soutenir. Le sport est d'une grande aide dans ce domaine, ce qui se passe sur un terrain peut très bien être appliqué dans le milieu professionnel.</p>
         </li>
         <li className="softSkillsContent__list">
           <img src={ Relaxation } alt="relaxation" />
           <h6>Gestion du stress:</h6> 
-          <p className="text">Etre commercial et maitre-chien m'ont aidé.</p>
+          <p className="text">Etre commercial et maitre-chien m'ont aidé. Il faut savoir rester maître de soi quand un chien vous fonce dessus et que votre rôle est de le faire mordre.</p>
         </li>
         <li className="softSkillsContent__list">
           <img src={ Empathie } alt="empathie" />
           <h6>Empathie:</h6> 
-          <p className="text">"Ressentir" les autres aide tous les jours dans les relations.</p>
+          <p className="text">"Ressentir" les autres aide tous les jours dans les relations. On peut sentir qu'un camarade est dans une impasse et lui apporter des solutions.</p>
         </li>
       </ul>      
     </div>
