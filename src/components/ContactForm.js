616cdaa7d192efaf130ed11e151bf10fe430eb71
Stéphane PIERRE <stephane_pierre@hotmail.fr>
2021-04-29 18:22:27 +0200

    STYLE add css and responsive

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,152 +1,194 @@
-import emailjs from 'emailjs-com';
-import React from 'react';
+ import emailjs from 'emailjs-com';
+ import React from 'react';
 
-function Field ({name, value, onChange, children}) {
+
+ function Field ({name, value, onChange, children}) {
+   return <div className="formBox">
+     <label htmlFor={name}>{children}</label>
+     <input type="text" value={value} onChange={onChange} id={name} name={name} className="formContent"/>
+   </div>
+ }
+ function FieldRequired ({name, value, onChange, children}) {
   return <div className="formBox">
     <label htmlFor={name}>{children}</label>
-    <input type="text" value={value} onChange={onChange} id={name} name={name} className="formContent"/>
+    <input type="text" value={value} onChange={onChange} required id={name} name={name} className="formContent"/>
   </div>
 }
-function CheckBox ({name, value, onChange, children}) {
-  return <div className="formBox">
-    <input type="checkbox" checked={value} onChange={onChange} id={name} name={name} className="formContent"/>
-    <label htmlFor={name}>{children}</label>
-  </div>
-}
-function EmailBox ({name, value, onChange, children}) {
+function FieldTel ({name, value, onChange, children}) {
   return <div className="formBox">
     <label htmlFor={name}>{children}</label>
-    <input type="mail" value={value} onChange={onChange} id={name} name={name} className="formContent"/>
+    <input type="tel" pattern="^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$" value={value} onChange={onChange} id={name} name={name} className="formContent"/>
   </div>
 }
-function TextArea ({name, value, onChange, children}) {
-  return <div className="formBox">
-    <label htmlFor={name}>{children}</label>
-    <textarea value={value} onChange={onChange} id={name} name={name} className="formContent"/>
-  </div>
-}
+ function CheckBox ({name, value, onChange, children}) {
+   return <div className="formBoxCheck">
+     <input type="checkbox" checked={value} onChange={onChange} id={name} name={name} className="formContent"/>
+     <label htmlFor={name}>{children}</label>
+   </div>
+ }
+ function EmailBox ({name, value, onChange, children}) {
+   return <div className="formBox">
+     <label htmlFor={name}>{children}</label>
+     <input type="email" pattern="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" value={value} onChange={onChange} required id={name} name={name} className="formContent"/>
+   </div>
+ }
+ function TextArea ({name, value, onChange, children}) {
+   return <div className="formBox">
+     <label htmlFor={name}>{children}</label>
+     <textarea rows="20" maxLength="2000" value={value} onChange={onChange} id={name} name={name} className="formContent"/>
+   </div>
+ }
 
 
+ export default class ContactForm extends React.Component {
+   constructor(props){
+     super(props)
+     this.state = {
+       lastName:"",
+       firstName:"",
+       company:"",
+       phone:"",
+       email:"",
+       natureOfTheRequest:"",
+       message:"",
+       motivation: false
+     }
 
-class ContactForm extends React.Component {
+     this.handleChange = this.handleChange.bind(this)
+     this.handleSubmit = this.handleSubmit.bind(this)
+   }
+   handleChange (e) {
+     const name = e.target.name
+     const type = e.target.type
+     const value = type === 'checkbox' ? e.target.checked : e.target.value
+     this.setState({
+       [name]: value
+     })
+   }
+   handleSubmit (e) {
+    e.preventDefault();
 
-  constructor(props){
-    super(props)
-    this.state = {
-      lastName:"",
-      firstName:"",
-      company:"",
-      phone:"",
-      email:"",
-      natureOfTheRequest:"",
-      message:"",
-      motivation: false
+    const {lastName, firstName, company, phone, email, natureOfTheRequest, message, motivation} = this.state
+
+    let templateParams = {
+      from_name: lastName,
+      to_name: 'service_93qv2no',
+      reply_to: email,
+      lastName: lastName,
+      firstName: firstName,
+      company: company,
+      phone: phone,
+      email: email,
+      natureOfTheRequest: natureOfTheRequest,
+      message: message,
+      motivation: motivation,
     }
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleChange (e) {
-    const name = e.target.name
-    const type = e.target.type
-    const value = type === 'checkbox' ? e.target.checked : e.target.value
-    this.setState({
-      [name]: value
+    fetch('https://api.emailjs.com/api/v1.0/email/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(templateParams)
     })
-  }
 
-  handleSubmit (e) {
-    e.preventDefault()
-
-    emailjs.sendForm('service_93qv2no','template_fqvizbo', e.target, 'user_bbkJTiBYlJBDDBKNLxGwT')
-      .then((result) => {
-        console.log(result.text);
+    .then((result) => {
+        console.log(result.statusText);
     }, (error) => {
-        console.log(error.text);
+        console.log(error.statusText);
     });
-
+   
+    e.target.reset();
+     emailjs.sendForm(
+       'service_93qv2no', 
+       'template_fqvizbo', 
+       e.target, 
+       'user_bbkJTiBYlJBDDBKNLxGwT'
+    )
+     .then((result) => {
+         console.log(result.text);
+     }, (error) => {
+         console.log(error.text);
+     });
+   }
+  resetForm(){
     this.setState({
-      lastName:"",
-      firstName:"",
-      company:"",
-      phone:"",
-      email:"",
-      natureOfTheRequest:"",
-      message:"",
-      motivation: false,
-    })
+     lastName:"",
+     firstName:"",
+     company:"",
+     phone:"",
+     email:"",
+     natureOfTheRequest:"",
+     message:"",
+     motivation: false     
+   })
   }
 
+   render() {
+     return (
+       <form className="container" onSubmit={this.handleSubmit}>
+         <h2>Contactez-moi</h2>
+         <div className="form-content">
+   {/* ............................................................................................ */}
+           <div className="lastNameContent">
+             <FieldRequired name="lastName" value={this.state.lastName} onChange={this.handleChange}>Nom *</FieldRequired>
+           </div> 
+   {/* ............................................................................................ */}
+           <div className="firstNameContent">
+             <Field name="firstName" value={this.state.firstName} onChange={this.handleChange}>Prénom</Field>
+           </div>
+   {/* ............................................................................................ */}
+           <div className="companyContent">
+             <Field name="company" value={this.state.company} onChange={this.handleChange}>Société</Field>
+           </div>
+   {/* ............................................................................................ */}
+           <div className="phoneContent">
+             <FieldTel name="phone" value={this.state.phone} onChange={this.handleChange}>Téléphone</FieldTel>
+           </div>
+   {/* ............................................................................................ */}
+           <div className="emailContent">
+             <EmailBox name="email" value={this.state.email} onChange={this.handleChange}>Email *</EmailBox>
+           </div>
+   {/* ............................................................................................ */}
+           <div className="requestContent">
+             <label 
+               className="request" 
+               htmlFor="request">
+                 Nature de la demande *
+             </label>
+             <select 
+               id="natureOfTheRequest"
+               name="natureOfTheRequest"
+               onChange={this.handleChange}
+               value={this.state.natureOfTheRequest} 
+               required 
+             >
+               <option value="">Sélectionnez</option>
+               <option value="stage">Proposition de stage</option>
+               <option value="alternating training">Proposition d'alternance</option>
+               <option value="job">Proposition d'emploi</option>
+               <option value="suggestion">Faire une suggestion</option>
+               <option value="curiosity">Juste par curiosité</option>
+               <option value="other">Autre sujet</option>
+             </select>
+           </div>
+   {/* ............................................................................................ */}
+           <div className="messageContent">
+             <TextArea name="message" value={this.state.message} onChange={this.handleChange}>Exprimez-vous</TextArea>
+           </div>
+   {/* ............................................................................................ */}
+           <div className="motivationContent">
+             <CheckBox name="motivation" value={this.state.motivation} onChange={this.handleChange}>Merci de cocher si le contact est de nature professionnelle</CheckBox>
+           </div> 
+   {/* ............................................................................................ */}
+           <input type="submit" value="Envoyer" className="button" />
+         </div>
+       </form>
+     )
+   }
+ }
 
-  render() {
-    return <div className="container" onSubmit={this.handleSubmit}>
-      <h2>Contactez-moi</h2>
 
-      <div className="form-content">
-{/* ............................................................................................ */}
-        <div className="lastNameContent">
-          <Field name="lastName" value={this.state.lastName} onChange={this.handleChange}>Nom *</Field>
-        </div> 
-{/* ............................................................................................ */}
 
-        <div className="firstNameContent">
-          <Field name="firstName" value={this.state.firstName} onChange={this.handleChange}>Prénom</Field>
-        </div>
-{/* ............................................................................................ */}
 
-        <div className="companyContent">
-          <Field name="company" value={this.state.company} onChange={this.handleChange}>Société</Field>
-        </div>
-{/* ............................................................................................ */}
-        <div className="phoneContent">
-          <Field name="phone" value={this.state.phone} onChange={this.handleChange}>Téléphone</Field>
-        </div>
-{/* ............................................................................................ */}
-        <div className="emailContent">
-          <EmailBox name="email" value={this.state.email} onChange={this.handleChange}>Email</EmailBox>
-        </div>
-{/* ............................................................................................ */}
-        <div className="requestContent">
-          {/* <SelectBox name="natureOfTheRequest" value={this.state.natureOfTheRequest} onChange={this.handleChange}>Nature de la demande */}
-          <label 
-            className="request" 
-            htmlFor="request">
-              Nature de la demande *
-          </label>
-          <select 
-            id="natureOfTheRequest"
-            name="natureOfTheRequest"
-            onChange={this.handleChange}
-            value={this.state.natureOfTheRequest}  
-          >
-            <option value="">Sélectionner un type</option>
-            <option value="stage">Proposition de stage</option>
-            <option value="alternating training">Proposition d'alternance</option>
-            <option value="job">Proposition d'emploi</option>
-            <option value="suggestion">Faire une suggestion</option>
-            <option value="curiosity">Juste par curiosité</option>
-            <option value="other">Autre sujet</option>
-          </select>
-          {/* </SelectBox> */}
-        </div>
-{/* ............................................................................................ */}
-        <div className="message">
-          <TextArea name="message" value={this.state.message} onChange={this.handleChange}>Exprimez-vous</TextArea>
-        </div>
-{/* ............................................................................................ */}
-        <div className="motivationContent">
-          <CheckBox name="motivation" value={this.state.motivation} onChange={this.handleChange}>Merci de cocher si le contact est de nature professionnel</CheckBox>
-        </div> 
-{/* ............................................................................................ */}
-
-        <div className="button">
-          Envoyer
-        </div>
-      </div>
-      {JSON.stringify(this.state)}
-    </div>
-  }
-}
-
-export default ContactForm;
